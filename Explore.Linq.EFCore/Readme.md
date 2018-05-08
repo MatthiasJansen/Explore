b48e350afbeea1f5f893b811e5e6290efff5af09
Matthias Jansen <matthias.jansen@outlook.de>
2018-05-08 19:27:58 +0200

    Update from the presentation;

--- Explore.Linq.EFCore/Readme.md
+++ Explore.Linq.EFCore/Readme.md
@@ -5,3 +5,12 @@
 A MSSQL Database with the current Adventure Works example schema.
 Can be created using the free tier on azure:
 [Azure Portal](https://portal.azure.com)
+
+# Parameters
+
+The application takes 4 parameters, which are:
+
+- DB Address
+- DB Schema
+- DB UserName
+- DB UserPass
--- Explore.Linq.EFCore/Scaffolding/PredicateBuilder.cs
+++ Explore.Linq.EFCore/Scaffolding/PredicateBuilder.cs
@@ -3,32 +3,32 @@ using System.Linq.Expressions;
 
 namespace Explore.Linq.EFCore.Scaffolding
 {
-    //public static class PredicateBuilder
-    //{
-    //    public static Expression<Func<TParam, bool>> True<TParam>()
-    //    {
-    //        return p => true;
-    //    }
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<TParam, bool>> True<TParam>()
+        {
+            return p => true;
+        }
 
-    //    public static Expression<Func<TParam, bool>> False<TParam>()
-    //    {
-    //        return p => false;
-    //    }
+        public static Expression<Func<TParam, bool>> False<TParam>()
+        {
+            return p => false;
+        }
 
-    //    public static Expression<Func<TParam, bool>> Or<TParam>(this Expression<Func<TParam, bool>> left,
-    //                                                            Expression<Func<TParam, bool>> right)
-    //    {
-    //        var invokedExpr = Expression.Invoke(right, left.Parameters);
-    //        return Expression.Lambda<Func<TParam, bool>>
-    //            (Expression.OrElse(left.Body, invokedExpr), left.Parameters);
-    //    }
+        public static Expression<Func<TParam, bool>> Or<TParam>(this Expression<Func<TParam, bool>> left,
+                                                                Expression<Func<TParam, bool>> right)
+        {
+            var invokedExpr = Expression.Invoke(right, left.Parameters);
+            return Expression.Lambda<Func<TParam, bool>>
+                (Expression.OrElse(left.Body, invokedExpr), left.Parameters);
+        }
 
-    //    public static Expression<Func<TParam, bool>> And<TParam>(this Expression<Func<TParam, bool>> left,
-    //                                                             Expression<Func<TParam, bool>> right)
-    //    {
-    //        var invokedExpr = Expression.Invoke(right, left.Parameters);
-    //        return Expression.Lambda<Func<TParam, bool>>
-    //            (Expression.AndAlso(left.Body, invokedExpr), left.Parameters);
-    //    }
-    //}
+        public static Expression<Func<TParam, bool>> And<TParam>(this Expression<Func<TParam, bool>> left,
+                                                                 Expression<Func<TParam, bool>> right)
+        {
+            var invokedExpr = Expression.Invoke(right, left.Parameters);
+            return Expression.Lambda<Func<TParam, bool>>
+                (Expression.AndAlso(left.Body, invokedExpr), left.Parameters);
+        }
+    }
 }
